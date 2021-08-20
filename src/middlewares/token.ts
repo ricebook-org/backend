780c5a130b8c70bcdabd9051811d9a5fafaf3171
alok8bb <alok8bb@gmail.com>
2021-08-21 02:58:13 +0530

    [feat:app] add token verification route

--- src/middlewares/token.ts
+++ src/middlewares/token.ts
@@ -1,5 +1,18 @@
+import { ErrorKind, HyError } from "hyougen";
 import { hyBodiedRouterMiddleware } from "hyougen/lib/routers";
+import User from "../models/User";
 
-export const verifyToken: hyBodiedRouterMiddleware<any> = (ctx) => {
-	console.log(ctx.request.get("token"));
+const TAG = "src/middlewares/token.ts";
+export const verifyToken: hyBodiedRouterMiddleware<any> = async (ctx, next) => {
+	const token = ctx.request.get("Authorization");
+	if (token == undefined) {
+		throw new HyError(ErrorKind.BAD_REQUEST, "Token not provided", TAG);
+	}
+
+	const existingUser = await User.findOne({ token }).lean();
+	if (existingUser == undefined) {
+		throw new HyError(ErrorKind.UNAUTHORIZED, "Invalid Token!", TAG);
+	}
+
+	await next();
 };
