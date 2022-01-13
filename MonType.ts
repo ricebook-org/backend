53b10dac1918552cc7eb1d49c6ab0ec639eca24c
Udit Karode <udit.karode@gmail.com>
2022-01-13 14:47:05 +0530

    chore: run montype

--- MonType.ts
+++ MonType.ts
@@ -1,28 +1,28 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export interface PostSchema {
-	title: string;
-	userId: string;
-	description: string;
-	imagePath: string;
-	grains: number;
-	tags: string[];
-	createdAt: Date;
-	updatedAt: Date;
-	_id: mongoose.Schema.Types.ObjectId;
-	id: string;
-}
+  title: string;
+  userId: string;
+  description: string;
+  imagePath: string;
+  grains: number;
+  tags: string[];
+  createdAt: Date;
+  updatedAt: Date;
+  _id: mongoose.Schema.Types.ObjectId;
+  id: string;
+};
 
 export interface UserSchema {
-	username: string;
-	email: string;
-	password: string;
-	otp: string;
-	isVerified: boolean;
-	token: string;
-	propicPath: string;
-	createdAt: Date;
-	updatedAt: Date;
-	_id: mongoose.Schema.Types.ObjectId;
-	id: string;
-}
+  username: string;
+  email: string;
+  password: string;
+  otp: string;
+  isVerified: boolean;
+  token: string;
+  propicPath: string;
+  createdAt: Date;
+  updatedAt: Date;
+  _id: mongoose.Schema.Types.ObjectId;
+  id: string;
+};
